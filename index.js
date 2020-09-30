761d17bd150f287a4b6b0b53b05f571b19ade56c
Astrid McKenzie <71366709+DaziePants@users.noreply.github.com>
2020-09-30 18:53:18 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -8,39 +8,27 @@ client.once('ready', () => {
 
 client.on('messageReactionAdd', async (reaction, user) => {
 	if (reaction.emoji.id === config.emojiID) {
-		if (reaction.message.partial) await reaction.message.fetch();
 		reaction.remove();
+		if (reaction.message.partial) await reaction.message.fetch();
 		if (reaction.message.content.length > 0) {
-			const reportEmbed = new Discord.MessageEmbed()
-			.setColor(`0x${config.embedColor}`)
-			.setTitle('Report :triangular_flag_on_post:')
-			.addFields(
-				{name: 'Message', value: reaction.message.content},
-				{name: 'Author', value: reaction.message.author, inline: true},
-				{name: 'Channel', value: `#${reaction.message.channel.name}`, inline: true},
-				{name: 'Reported By', value: user, inline: true},
-				{name: 'Link', value: `[Go to Message](https://discordapp.com/channels/${config.guildID}/${reaction.message.channel.id}/${reaction.message.id}) :arrow_right:`},
-				)
-			.setTimestamp()
-			.setFooter('React with 👍 to acknowldge')
-			client.channels.cache.get(config.channelID).send(reportEmbed);
-			user.send(config.message);
+			var message = reaction.message.content
 		} else {
-			const reportEmbed = new Discord.MessageEmbed()
-			.setColor(`0x${config.embedColor}`)
-			.setTitle('Report :triangular_flag_on_post:')
-			.addFields(
-				{name: 'Message', value: `https://discordapp.com/channels/${config.guildID}/${reaction.message.channel.id}/${reaction.message.id}`},
-				{name: 'Author', value: reaction.message.author, inline: true},
-				{name: 'Channel', value: `#${reaction.message.channel.name}`, inline: true},
-				{name: 'Reported By', value: user, inline: true},
-				{name: 'Link', value: `[Go to Message](https://discordapp.com/channels/${config.guildID}/${reaction.message.channel.id}/${reaction.message.id}) :arrow_right:`},
-				)
-			.setTimestamp()
-			.setFooter('React with 👍 to acknowldge')
-			client.channels.cache.get(config.channelID).send(reportEmbed);
-			user.send(config.message);
+			var message = 'Embedded Content'
 		}
+		const reportEmbed = new Discord.MessageEmbed()
+		.setColor(`0x${config.embedColor}`)
+		.setTitle('Report :triangular_flag_on_post:')
+		.addFields(
+			{name: 'Message', value: message},
+			{name: 'Author', value: reaction.message.author, inline: true},
+			{name: 'Channel', value: `#${reaction.message.channel.name}`, inline: true},
+			{name: 'Reported By', value: user, inline: true},
+			{name: 'Link', value: `[Go to Message](https://discordapp.com/channels/${config.guildID}/${reaction.message.channel.id}/${reaction.message.id}) :arrow_right:`},
+			)
+		.setTimestamp()
+		.setFooter('React with 👍 to acknowldge')
+		client.channels.cache.get(config.channelID).send(reportEmbed);
+		user.send(config.message);
 		return;
 	}
 });
